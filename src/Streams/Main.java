7d1c7501097894bcad692857d2040e30480b20ed
Lei Justine <clemente.leijustine@gmail.com>
2026-07-22 19:00:01 +0800

    Refresh lambda and streams

--- src/Streams/Main.java
+++ src/Streams/Main.java
@@ -1,27 +1,22 @@
  package Streams;
 
  import java.util.*;
- import java.util.stream.Collectors;
 
  public class Main{
      public static void main(String[] args){
-          // filter grade >= 85
-          List<Person> students = new ArrayList<>();
-         students.add(new Person("alice" , 95.0));
-         students.add(new Person("jason" , 90.0));
-         students.add(new Person("lei" , 85.0));
-         students.add(new Person("Athasia" , 81.0));
-         students.add(new Person("jake" , 72.0));
+         // filter grade >= 85
+         List<Person> students = new ArrayList<>();
+         students.add(new Person("Lei" , 54.6));
+         students.add(new Person("Juan" , 89.6));
+         students.add(new Person("Athasia" , 97.6));
+         students.add(new Person("Cruz" , 90.6));
 
-            ArrayList<Person> Students_higher = students.stream().filter(p -> p.grade >= 85.0)
-                    .map(p -> new Person(p.Student_name.toUpperCase() , p.grade))
-                    .sorted( Comparator.comparing(p -> p.Student_name))
-                    .collect(Collectors.toCollection(ArrayList::new));
-
-            Students_higher.forEach(i ->{
-                System.out.println(i.Student_name + ": " + i.grade);
-                System.out.println("------------------------------");
-            });
+         // filter map sorted and collect
+         List<Person> newList = students.stream().filter((s) -> s.grade >= 85.0)
+                 .sorted(Comparator.comparing((s) -> s.Student_name))
+                 .toList();
+         // loop
+         newList.forEach((i) -> System.out.println(i.Student_name + " " + i.grade));
 
      }
  }
